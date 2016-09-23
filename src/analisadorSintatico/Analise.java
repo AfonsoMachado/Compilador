be5de2c30ca86ecaa2ceb3976374742dc50f8d76
afonso <afonso@jabba>
2016-09-23 14:53:20 -0300

    Constantes dando erro

--- src/analisadorSintatico/Analise.java
+++ src/analisadorSintatico/Analise.java
@@ -20,7 +20,7 @@ public class Analise {
 				v = declaracaoVariaveis(tokens, pos+1, 1);
 				pos = v.getPosicao()+1;
 				principal(tokens, pos);
-			case "constante":	
+			case "const":	
 				v = declaracaoConstante(tokens, pos+1, 1);
 				pos = v.getPosicao()+1;
 				principal(tokens, pos);
@@ -40,10 +40,65 @@ public class Analise {
 			System.out.println();
 			switch (cont){
 			case 1: 
-
-
-
-			}
+				if (t.getTipo().equals("Palavra Reservada")){
+					if (verif.tipo(t.getLexema())){
+						Vasculhador v1 = new Vasculhador();
+						posicao ++;
+						cont ++;
+						v1 = declaracaoConstante(tokens, posicao, cont);					
+						posicao = v1.getPosicao();
+						cont = v1.getCont();
+						return v1;
+					} else {
+						erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
+					}
+				} else {
+					erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
+				}
+			case 2:
+				if (t.getTipo().equals("Identificador")){
+					Vasculhador v1 = new Vasculhador();
+					posicao ++;
+					cont ++;
+					v1 = declaracaoConstante(tokens, posicao, cont);	
+					posicao = v1.getPosicao();
+					cont = v1.getCont();
+					return v1;
+				} else {
+					erro(posicao + "falta de identificador" );
+				}
+			case 3:
+				if (t.getTipo().equals("Operador Relacional")){
+					if (t.getLexema().equals("=")){
+						Vasculhador v1 = new Vasculhador();
+						posicao ++;
+						cont ++;
+						v1 = declaracaoConstante(tokens, posicao, cont);	
+						posicao = v1.getPosicao();
+						cont = v1.getCont();
+						return v1;
+					} else {
+						erro("Operador relacional incorreto");
+					}
+				} else {
+					erro(posicao + "falta de operador relacional");
+				}
+			case 4: //VALOR
+				
+			case 5:
+				if (t.getTipo().equals("Delimitador")){
+					if (t.getLexema().equals(";")){	
+						Vasculhador v1 = new Vasculhador();
+						v1.setCont(cont++);
+						v1.setPosicao(posicao ++);
+						return v1;
+					} else {
+						erro("Delimitador incorreto");
+					}
+				} else {
+					erro("Inexistencia de um delimitador");
+				}
+			} // fim do switch case
 		} else {
 			System.out.println("erro");
 		}
@@ -73,7 +128,9 @@ public class Analise {
 						erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
 					}
 
-				}					
+				} else {
+					erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
+				}
 			case 2: 
 				if (t.getTipo().equals("Identificador")){
 					Vasculhador v1 = new Vasculhador();
@@ -111,6 +168,10 @@ public class Analise {
 	private void erro(String erro) {
 		erros.add(erro);
 		System.out.println("Erro Karai!   " + erro );
+	}
+	
+	public ArrayList<String> getErros() {
+		return this.erros;
 	}
 
 }
