c4dd11c21bfaa55e6e32d35ee956c9cd99630009
afonso <afonso@jabba>
2016-09-23 12:17:11 -0300

    Identando código fonte

--- src/analisadorSintatico/Analise.java
+++ src/analisadorSintatico/Analise.java
@@ -5,225 +5,112 @@ import java.util.ArrayList;
 import analisadorLexico.Token;
 
 public class Analise {
-	
-		
-		private Verificacoes verif = new Verificacoes();
-		private Vasculhador v = new Vasculhador();
-		private ArrayList<String> erros = new ArrayList<String>();
-	
-	
+
+	private Verificacoes verif = new Verificacoes();
+	private Vasculhador v = new Vasculhador();
+	private ArrayList<String> erros = new ArrayList<String>();
+
 	public int principal(ArrayList<Token> tokens, int pos) {
-		
-	
-			if (pos < tokens.size()){
-		
-		v.setPosicao(pos);
-		Token t = tokens.get(pos);
-		
-		
-		switch (t.getLexema()){
-		
-		
-		case "var":
-							
-			v = declaracaoVariaveis(tokens, pos+1, 1);
-			
-			 pos = v.getPosicao()+1;
-			principal(tokens, pos);
-			
-		case "constante":	
-			
-			v = declaracaoConstante(tokens, pos+1, 1);
-			pos = v.getPosicao()+1;
-			principal(tokens, pos);
-			 
-		case "leia": pos = verificacaoLeia (tokens, pos++, 1);	
-		
+		if (pos < tokens.size()){
+			v.setPosicao(pos);
+			Token t = tokens.get(pos);
+
+			switch (t.getLexema()){
+			case "var":							
+				v = declaracaoVariaveis(tokens, pos+1, 1);
+				pos = v.getPosicao()+1;
+				principal(tokens, pos);
+			case "constante":	
+				v = declaracaoConstante(tokens, pos+1, 1);
+				pos = v.getPosicao()+1;
+				principal(tokens, pos);
+			case "leia": 
+				pos = verificacaoLeia (tokens, pos++, 1);	
 				principal(tokens, pos +1);
-		}
-		
-		
-		
-		
-			}else{
-				
-				System.out.println("Fim do arquivo");
 			}
-			
-			
-		
+		} else {
+			System.out.println("Fim do arquivo");
+		}
 		return pos;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	private Vasculhador declaracaoConstante(ArrayList<Token> tokens, int posicao, int cont) {
 
+	private Vasculhador declaracaoConstante(ArrayList<Token> tokens, int posicao, int cont) {
 		if(posicao < tokens.size() ){
-			
-			
 			Token t = tokens.get(posicao);
 			System.out.println();
-			
-			
-			
 			switch (cont){
-			
 			case 1: 
-			
-			
-			
+
+
+
 			}
-			
-		}else{
-			
+		} else {
 			System.out.println("erro");
 		}
-		
-		
-		
 		return null;
 	}
 
-
-
-
-
-
-
-
-
-
 	private int verificacaoLeia(ArrayList<Token> tokens, int posicao, int cont) {
-		
-		
 		return posicao;
 	}
 
-
-
-
-
-
-
-
-
-
 	private Vasculhador declaracaoVariaveis(ArrayList<Token> tokens, int posicao, int cont){
-		
-	
 		if(posicao < tokens.size() ){
-			
-		
-		Token t = tokens.get(posicao);
-		System.out.println();
-		
-		
-		
-		switch (cont){
-		
-		case 1: if (t.getTipo().equals("Palavra Reservada")){
-			
+			Token t = tokens.get(posicao);
+			System.out.println();
+			switch (cont){
+			case 1: 
+				if (t.getTipo().equals("Palavra Reservada")){
 					if (verif.tipo(t.getLexema())){
-						
 						Vasculhador v1 = new Vasculhador();
 						posicao ++;
 						cont ++;
 						v1 = declaracaoVariaveis(tokens, posicao, cont);					
-						
-						
 						posicao = v1.getPosicao();
 						cont = v1.getCont();
 						return v1;
-					 
-					}else{
-						
-						 erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
+					} else {
+						erro("falta palavra reservada: inteiro, cadeia, real, booleano, caractere");
 					}
-		
+
 				}					
-					
-		
-		case 2: 
-			
-			if (t.getTipo().equals("Identificador")){
-			
-			
-					
-			
-				Vasculhador v1 = new Vasculhador();
-				
-				posicao ++;
-				cont ++;
-				
-				v1 = declaracaoVariaveis(tokens, posicao, cont);	
-	
-				posicao = v1.getPosicao();
-				cont = v1.getCont();
-				return v1;
-			 
-				}else{
-	
+			case 2: 
+				if (t.getTipo().equals("Identificador")){
+					Vasculhador v1 = new Vasculhador();
+					posicao ++;
+					cont ++;
+					v1 = declaracaoVariaveis(tokens, posicao, cont);	
+					posicao = v1.getPosicao();
+					cont = v1.getCont();
+					return v1;
+				} else {
 					erro(posicao + "falta de identificador" );
 				}
-		
-		case 3 :			
-			
-			
-			if (t.getTipo().equals("Delimitador")){
-				if (t.getLexema().equals(";")){	
-					Vasculhador v1 = new Vasculhador();
-					v1.setCont(cont++);
-					v1.setPosicao(posicao ++);
-					return v1;
+			case 3 :			
+				if (t.getTipo().equals("Delimitador")){
+					if (t.getLexema().equals(";")){	
+						Vasculhador v1 = new Vasculhador();
+						v1.setCont(cont++);
+						v1.setPosicao(posicao ++);
+						return v1;
+					}
+					if (t.getLexema().equals(",")){	
+						Vasculhador v1 = new Vasculhador();
+						cont = cont -1;
+						posicao ++;
+						return declaracaoVariaveis(tokens, posicao, cont);
+					}	
 				}
-				if (t.getLexema().equals(",")){	
-					Vasculhador v1 = new Vasculhador();
-					cont = cont -1;
-					posicao ++;
-					
-					
-					return declaracaoVariaveis(tokens, posicao, cont);
-					
-				}	
-				
-				
-				
-				
 			}
-			
-			
-			
-			
-		}
-		
-		}else{
-			
+		} else {
 			System.out.println("erro");
 		}
 		return null;
-	
-		
-		
-		
 	}
 
-
-
 	private void erro(String erro) {
-		
 		erros.add(erro);
-		
 		System.out.println("Erro Karai!   " + erro );
-		
-		
 	}
 
 }
